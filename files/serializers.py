b7eb2a97f9b3e74155e2649a3886a0f0c12c50ca
davidcasr <david.castroruiz@gmail.com>
2020-09-18 19:12:24 -0500

    Upload multiple files in multiple implementations | Install CORS

--- files/serializers.py
+++ files/serializers.py
@@ -1,6 +1,21 @@
 from rest_framework import serializers
 from files.models import File
 
+class FileListSerializer(serializers.Serializer) :
+    name = serializers.CharField(max_length=50)
+    one_file = serializers.ListField(
+                       child=serializers.FileField( max_length=100000,
+                                         allow_empty_file=False,
+                                         use_url=False )
+                                )
+    
+    def create(self, validated_data):
+        name=validated_data.pop('name')
+        one_file=validated_data.pop('one_file')
+        for file in one_file:
+            f = File.objects.create(name=name, one_file=file,**validated_data)
+        return f
+
 class FileSerializer(serializers.ModelSerializer):
     class Meta:
         model = File  
--- files/views.py
+++ files/views.py
@@ -2,13 +2,17 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import FileSerializer
+from .serializers import FileListSerializer
 from .models import File
-from rest_framework import status
+from rest_framework import status, permissions
 from django.http import Http404
 from rest_framework.parsers import FileUploadParser
+from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
     
 class Files_APIView(APIView):
-    parser_class = (FileUploadParser,)
+    #parser_class = (FileUploadParser, )
+    parser_classes = (MultiPartParser, FormParser, JSONParser)
+    #permission_classes = (permissions.AllowAny,)
 
     def get(self, request, format=None, *args, **kwargs):
         file = File.objects.all()
@@ -16,12 +20,46 @@ class Files_APIView(APIView):
         
         return Response(serializer.data)
 
-    def post(self, request, format=None):
-        serializer = FileSerializer(data=request.data)
+    """
+        Post implementation #1
+            - Save one file
+            - Use FileSerializer
+    """
+    # def post(self, request, format=None):
+    #     serializer = FileSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    """
+        Post implementation #2
+            - Save multiple files
+            - Use FileSerializer
+            - Idea: https://docs.djangoproject.com/en/3.0/topics/http/file-uploads/#uploading-multiple-files
+    """
+    # def post(self, request, format=None):
+    #     serializer = FileSerializer(data=request.data)
+        
+        files_list = request.FILES.getlist('one_file')
         if serializer.is_valid():
-            serializer.save()
+            for item in files_list:
+                f = File.objects.create(name=request.data['name'], one_file=item)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    """
+        Post implementation #3
+            - With error: AttributeError
+            - Save multiple files
+            - Use FileListSerializer
+    """
+    # def post(self, request, format=None):
+    #     serializer = FileListSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
 
 class Files_APIView_Detail(APIView):
 
